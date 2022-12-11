17e173115565ff17289634904c81ed5d1abc0438
TichaTin <ticha.tin@gmail.com>
2022-12-11 23:01:53 +1100

    Set up Home + Login router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from 'react';
+import {Routes, Route, useNavigate} from 'react-router-dom';
+
+import Login from './components/Login';
+import Home from './container/Home';
 
 const App = () => {
   return (
-    <div className='text-3xl font-bold underline'>
-      App
-    </div>
+    <Routes>
+      <Route path='login' element={<Login />}/>
+      <Route path='/*' element={<Home />}/>
+    </Routes>
   )
 }
 
