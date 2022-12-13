3454c274ed36e3eaf546af842ef769ed34096f53
TichaTin <ticha.tin@gmail.com>
2022-12-14 09:57:30 +1100

    Pin section layout + components added

--- frontend/src/container/Pins.jsx
+++ frontend/src/container/Pins.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Routes,Route } from 'react-router-dom';
 
-const Pins = () => {
+// components
+
+import { Navbar, Feed, PinDetails, CreatePin, Search } from '../components';
+
+const Pins = ({user}) => {
+  const [searchTerm, setSearchTerm] = useState(''); // to share it accross multi component
+
   return (
-    <div>Pins</div>
+    <div className='px-2 md:px-5'>
+      <div className='bg-gray-50'>
+        <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+      </div>
+      <div className='h-full'>
+        <Routes>
+          <Route path='/' element={<Feed/>} />
+          <Route path='/category/:categoryId' element={<Feed/>} />
+          <Route path='/pin-detail/:pinId' element={<PinDetails user={user && user}/>} />
+          <Route path='/create-pin' element={<CreatePin user={user && user}/>} />
+          <Route path='/search' element={<Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>} />
+        </Routes>
+      </div>
+    </div>
   )
 }
 
