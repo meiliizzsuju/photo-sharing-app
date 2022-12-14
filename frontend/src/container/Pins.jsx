fbb1a84667251fbebcad33cded85d5f9d46686de
TichaTin <ticha.tin@gmail.com>
2022-12-14 13:43:01 +1100

    Nav + search bar layout

--- frontend/src/container/Pins.jsx
+++ frontend/src/container/Pins.jsx
@@ -11,7 +11,7 @@ const Pins = ({user}) => {
   return (
     <div className='px-2 md:px-5'>
       <div className='bg-gray-50'>
-        <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+        <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} user={user&&user}/>
       </div>
       <div className='h-full'>
         <Routes>
