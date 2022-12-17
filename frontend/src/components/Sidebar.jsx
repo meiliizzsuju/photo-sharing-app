3903ec56417b43728b84b360281c4b7b7a3d777e
TichaTin <ticha.tin@gmail.com>
2022-12-17 16:43:12 +1100

    import dynamic sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -4,31 +4,11 @@ import { RiHomeFill } from 'react-icons/ri';
 import { IoIosArrowForward } from 'react-icons/io';
 
 import logo from '../assets/logo.png'
+import { categories } from '../utils/data';
 
 
 const isNotActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
 const isActiveStyle = 'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize';
-
-const categories = [
-  {
-    name: 'Animals'
-  },
-  {
-    name: 'Wallpapers'
-  },
-  {
-    name: 'Photography'
-  },
-  {
-    name: 'Gaming'
-  },
-  {
-    name: 'Coding'
-  },
-  {
-    name: 'Other'
-  },
-]
 
 const Sidebar = ({user,closeToggle}) => {
   // call 'closeToggle' from parent class
